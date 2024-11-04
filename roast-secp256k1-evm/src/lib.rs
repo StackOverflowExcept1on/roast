b6d6d81c9316b88e43bbf150edd965b384ae7dab
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-11-04 12:56:09 +0300

    switch to crates io instead of git dependencies

---
+++ roast-secp256k1-evm/src/lib.rs
@@ -0,0 +1,46 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(missing_docs)]
+#![doc = include_str!("../README.md")]
+#![doc = document_features::document_features!()]
+
+mod coordinator {
+    /// Represents all possible session statuses.
+    pub type SessionStatus = roast_core::SessionStatus<frost_secp256k1_evm::Secp256K1Keccak256>;
+
+    /// Represents coordinator.
+    pub type Coordinator = roast_core::Coordinator<frost_secp256k1_evm::Secp256K1Keccak256>;
+}
+
+pub mod dkg {
+    //! Distributed Key Generation types.
+
+    pub use roast_core::dkg::DkgStatus;
+
+    /// Represents dealer that can be used for Distributed Key Generation.
+    pub type Dealer = roast_core::dkg::Dealer<frost_secp256k1_evm::Secp256K1Keccak256>;
+
+    /// Represents participant of Distributed Key Generation.
+    pub type Participant = roast_core::dkg::Participant<frost_secp256k1_evm::Secp256K1Keccak256>;
+}
+
+pub mod error {
+    //! Error types.
+
+    /// Represents all possible errors that can occur in FROST protocol.
+    pub type FrostError = roast_core::error::FrostError<frost_secp256k1_evm::Secp256K1Keccak256>;
+
+    pub use roast_core::error::{DkgError, MaliciousSignerError, RoastError};
+
+    /// Represents all possible errors that can occur.
+    pub type Error = roast_core::error::Error<frost_secp256k1_evm::Secp256K1Keccak256>;
+}
+
+mod signer {
+    /// Represents signer.
+    pub type Signer = roast_core::Signer<frost_secp256k1_evm::Secp256K1Keccak256>;
+}
+
+pub use frost_secp256k1_evm as frost;
+
+pub use coordinator::*;
+pub use signer::*;
